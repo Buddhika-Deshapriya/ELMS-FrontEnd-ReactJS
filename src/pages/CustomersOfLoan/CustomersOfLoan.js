3094cc13bdd3adf43d991267a1e11469b55a4b8d
buddhika deshapriya <buddhika384@gmail.com>
2020-09-13 22:01:11 +0530

    set collapse in CustomersOfLoan page

--- src/pages/CustomersOfLoan/CustomersOfLoan.js
+++ src/pages/CustomersOfLoan/CustomersOfLoan.js
@@ -9,7 +9,11 @@ import {
     TableContainer, TableHead, TableRow, Tooltip, Container
 } from '@material-ui/core';
 import { Grid, Paper } from '@material-ui/core';
-
+import Collapse from '@material-ui/core/Collapse';
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
 
 import AppTemplate from '../Templates/AppTemplate/AppTemplate';
 import { appConfig } from '../../configs/app.config';
@@ -43,7 +47,7 @@ const StyledTableRow = withStyles((theme) => ({
     },
 }))(TableRow);
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
         maxWidth: 450,
     },
@@ -58,14 +62,29 @@ const useStyles = makeStyles({
     },
     table: {
         maxWidth: 500,
+    },
+    expand: {
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        color:red,
     }
-});
+}));
 
 export default function ViewLoanCustomerData(props) {
 
 
     const classes = useStyles();
     const [CustomersData, ViewCustomerData] = useState([]);
+
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
 
     const CustomerId = props.match.params.id;
 
@@ -125,7 +144,15 @@ export default function ViewLoanCustomerData(props) {
 
                                             CustomersData.map((row) => (
                                                 <StyledTableRow key={row.id}>
-                                                    <StyledTableCell align="left">{row.membership_no}</StyledTableCell>
+                                                    <StyledTableCell
+                                                        className={clsx(classes.expand, {
+                                                            [classes.expandOpen]: expanded,
+                                                        })}
+                                                        onClick={handleExpandClick}
+                                                        aria-expanded={expanded}
+                                                        aria-label="show more" align="left">
+                                                            {row.membership_no}
+                                                        </StyledTableCell>
                                                     <StyledTableCell align="left">{row.first_name}{" "} {row.middle_name}{" "}{row.last_name}</StyledTableCell>
                                                     <StyledTableCell align="left">{row.nic}</StyledTableCell>
                                                 </StyledTableRow>
@@ -136,7 +163,35 @@ export default function ViewLoanCustomerData(props) {
                         </TableContainer>
                     </Grid>
                     <Grid item xs={7}>
-                        
+                        <Card>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <Typography paragraph>Method:</Typography>
+                                    <Typography paragraph>
+                                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                                        minutes.
+                                </Typography>
+                                    <Typography paragraph>
+                                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                                        heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                                        browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                                        and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                                        pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                                </Typography>
+                                    <Typography paragraph>
+                                        Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                                        without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                                        medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                                        again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                                        minutes more. (Discard any mussels that don’t open.)
+                                </Typography>
+                                    <Typography>
+                                        Set aside off of the heat to let rest for 10 minutes, and then serve.
+                                 </Typography>
+                                </CardContent>
+                            </Collapse>
+                        </Card>
                     </Grid>
                 </Grid>
             </div>

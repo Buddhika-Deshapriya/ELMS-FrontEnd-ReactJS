f0561dbb2c603465e92ed9a84bdcebbbba2201fd
buddhika deshapriya <buddhika384@gmail.com>
2021-05-02 19:02:59 +0530

    set image to home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,7 +2,15 @@ import React, { useEffect } from 'react';
 
 import AppTemplate from '../Templates/AppTemplate/AppTemplate';
 import { appConfig } from '../../configs/app.config';
+
+import {
+  Button, Paper, Grid, TextField, InputLabel, Select, FormControl,
+  FormHelperText, MenuItem
+} from '@material-ui/core';
+
+import logo from './coop test2.jpg';
 import utils from '../../helper/utils';
+import { Height } from '@material-ui/icons';
 const { baseUrl } = appConfig;
 
 
@@ -10,7 +18,15 @@ export default function HomePage(props) {
   return (
       <AppTemplate>
         <div className="home-page">
-        <h1> Home page.</h1>
+        {/* <h1> Home page.</h1> */}
+        <Grid>
+          <img src={logo} width="1400" />
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            
+          </Grid>
+        </Grid>
         </div>
       </AppTemplate>
   )
